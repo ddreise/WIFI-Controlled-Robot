a4f9558b704b4b6ad0a5a38964b650a6409d7fcb
Valend <Valend@users.noreply.github.com>
2019-09-19 13:19:36 -0400

    laid out proper test code for activaating peripherals -- only LCD screen is verified to work.

--- main.c
+++ main.c
@@ -32,6 +32,14 @@
 
 #define RS232_Interface	1
 
+//Prototypes
+void LCD_Test(void);
+void DC_Motor_Test(void);
+void Encoder_Test(void);
+void Servo_Test(void);
+void Stepper_Test(void);
+void Beeper_Test(void);
+
 int main(void){
 
 	uint16_t counter;
@@ -261,6 +269,11 @@ int main(void){
 		}
 		
 		UART1_init();
+		RC_Init();
+		Encoder_Init();
+		TIM3_Init();
+		stepperInit();
+		LimSwitch_Init();
 		
 		//send menu to host
 		Test_Menu();
@@ -268,17 +281,120 @@ int main(void){
 		//get user input
 		get_Input(str);
 		
+		LCDclear(); //need to clear LCD so that there is no left over characters
+		
 		LCD_Printf(SECOND_LINE, "%s", str); //output input to LCD screen for reference
 		//perform correct action based on input
-		if(!strcmp(str, "1"))
-		{
-			LCD_Printf(FIRST_LINE, "LCD GO!");
-		}
+		if(!strcmp(str, "1")) LCD_Test();
+		else if(!strcmp(str, "2")) DC_Motor_Test();
+		else if(!strcmp(str, "3")) Encoder_Test();
+		else if(!strcmp(str, "4")) Servo_Test();
+		else if(!strcmp(str, "5")) Stepper_Test();
+		else if(!strcmp(str, "6")) Beeper_Test();
 		
 		//keep performing action
 	}
 	
 	#endif
-	
+
 }
 
+void LCD_Test()
+{
+	LCD_Printf(FIRST_LINE, "LCD test go!");
+}
+
+void DC_Motor_Test()
+{
+	Delay_s(5);
+	Motor(DC_M1, 50, DC_FORWARD);
+	Motor(DC_M2, 50, DC_FORWARD);
+	Delay_s(5);
+	Motor(DC_M1, 100, DC_FORWARD);
+	Motor(DC_M2, 100, DC_FORWARD);
+	Delay_s(5);
+		
+	Motor(DC_M2, 100, DC_COAST);
+	Motor(DC_M1, 100, DC_COAST);
+	Delay_ms(200);
+
+	Motor(DC_M1, 50, DC_BACKWARD);
+	Motor(DC_M2, 50, DC_BACKWARD);
+	Delay_s(5);
+	Motor(DC_M1, 100, DC_BACKWARD);
+	Motor(DC_M2, 100, DC_BACKWARD);
+	Delay_s(5);
+	
+	//coasting speed doesn't matter because inputs need to be low anyways
+	Motor(DC_M1, 100, DC_COAST);
+	Motor(DC_M2, 100, DC_COAST);
+	
+	Delay_s(5);
+	Motor(DC_M1, 50, DC_BRAKE);
+	Motor(DC_M2, 50, DC_BRAKE);
+	Delay_s(5);
+	Motor(DC_M1, 100, DC_BRAKE);
+	Motor(DC_M2, 100, DC_BRAKE);
+}
+
+void Encoder_Test()
+{
+	uint32_t pulseL;
+	uint32_t pulseR;
+	
+	LCD_Printf(FIRST_LINE, "Encoder:");
+	
+	while(TRUE)
+	{
+		pulseL = Encoder_Read(ENCODER_LEFT);
+		pulseR = Encoder_Read(ENCODER_RIGHT);
+		
+		LCD_Printf(SECOND_LINE, "EL %ldms ER %ldms ", pulseL, pulseR);
+		Delay_ms(50);
+	}
+}
+
+void Servo_Test()
+{
+	LCD_Printf(FIRST_LINE, "RC SERVO");
+	
+	while(TRUE)
+	{
+		RC_Position(0);
+		Delay_s(5);
+		RC_Position(89);		// In testing, Servo could not exceed +89 degrees. If > 89 degrees, the gears grind.
+		Delay_s(5);
+		RC_Position(-90);
+		Delay_s(5);
+	}
+}
+
+void Stepper_Test()
+{
+	LCD_Printf(FIRST_LINE, "STEPPER");
+	
+	Stepper_Home();
+	
+	Delay_s(5);
+	
+	Stepper_Set(STPR_FULL, 0x00FF, 90);
+	
+	Delay_s(5);
+	
+	Stepper_Set(STPR_FULL, 0x0FFF, -90);
+	
+	Delay_s(5);
+	
+	Stepper_Set(STPR_FULL, 0xFFFF, 0);
+	
+	while(TRUE)
+	{
+		LCD_Printf(SECOND_LINE, "Stepper Pos: %d ", Get_Stepper_Position());
+		Delay_ms(20);
+	}
+}
+
+void Beeper_Test()
+{
+	
+}
