dd3b8cb0ee6f8b5177db47d582fb6c716fc6bfef
Daniel Dreise <Ddreise6630@conestogac.on.ca>
2019-11-15 12:09:19 -0500

    DAC configuration

--- Robot/DAC.c
+++ Robot/DAC.c
@@ -3,3 +3,27 @@
 // Implementation of a Digital-to-Analog Converter in order to physically display speeds as voltages
 //
 // 
+
+#include <stdio.h>
+#include "DAC.h"
+#include "Control_Law.h"
+#include "Encoder.h"
+#include "Macros.h"
+
+void DAC_Init(){
+	
+	GPIO_PORT_ENABLE(DAC_PORT);									// Enable output port
+	
+	GPIO_PIN_MODE(DAC_PORT, DAC_PIN, ANALOG);		// Set as analog output
+	
+	SET_BITS(DAC->CR, DAC_CR_EN1);							// Enable DAC1
+	
+	CLR_BITS(DAC->DHR8R1, DAC_DHR8R1_DACC1DHR);	// Clear output register
+	
+}
+
+void DAC_output(int8_t wheel_speed){
+	
+	FORCE_BITS(DAC->DHR8R1, DAC_DHR8R1_DACC1DHR, wheel_speed);
+	
+}
--- Robot/DAC.h
+++ Robot/DAC.h
@@ -2,4 +2,12 @@
 #ifndef DAC_H
 #define DAC_H
 
+#include "stm32f303xe.h"
+
+#define DAC_PORT A
+#define DAC_PIN 4
+
+void DAC_Init(void);
+void DAC_output(int8_t wheel_speed);
+
 #endif
